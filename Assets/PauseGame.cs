a7b25a6d7d6ca7941250813d9e210ebe7f0c568d
reya <minrvei@gmail.com>
2025-12-04 14:43:22 +0700

    LVL 2
    LVL 2, Setting blm

--- Assets/PauseGame.cs
+++ Assets/PauseGame.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
     {
-        
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Resume()
     {
-        
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
+
 }
---
+++ Assets/UIManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    AudioManagment audioManager;
+    //public Animator animator;
+    //public GameObject Play;
+    public GameObject Settings;
+    public GameObject SettingsPanel;
+    //public GameObject Exit;
+    public GameObject Close;
+
+    //private Button PlayButton;
+    private Button SettingButton;
+    private Button ExitButton;
+    private Button CloseButton;
+
+
+    private void Awake()
+    {
+        //PlayButton = Play.GetComponent<Button>();
+        SettingButton = Settings.GetComponent<Button>();
+        //ExitButton = Exit.GetComponent<Button>();
+        CloseButton = Close.GetComponent<Button>();
+
+        //audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManagment>();
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManagment>();
+
+
+        SettingsPanel.gameObject.SetActive(false);
+    }
+    private void Start()
+    {
+        //PlayButton.onClick.AddListener(() =>
+        //{
+        //    audioManager.PlaySFX(audioManager.buttonClick);
+        //});
+
+        SettingButton.onClick.AddListener(() =>
+        {
+            audioManager.PlaySFX(audioManager.buttonClick);
+            SettingsPanel.gameObject.SetActive(true);
+            //animator.SetTrigger(SETTINGOPENING_ANIMATION);
+
+        });
+
+        CloseButton.onClick.AddListener(() =>
+        {
+            audioManager.PlaySFX(audioManager.buttonClick);
+        });
+
+        //ExitButton.onClick.AddListener(() =>
+        //{
+        //    audioManager.PlaySFX(audioManager.buttonClick);
+        //    Application.Quit();
+        //});
+    }
+}
